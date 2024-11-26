a894ab37fd731af76d0f26a77e2a5baf6bf4ec6e
rafaella godinho <rafaellagodinho@outlook.com>
2024-11-26 17:17:18 -0300

    knex config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
-import fastify from "fastify";
+import fastify from 'fastify';
+import { knex } from './database';
 
 const app = fastify();
 
-app.get("/", () => {
-  return "Hello World";
-});
+async function setup() {
+  await knex.raw('SELECT 1');
+}
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
+})
+
+setup()
+  .then(() => {
+    app.listen({ port: 3333 }).then(() => {
+      console.log("Server is running on port 3333");
+    });
+  })
+  .catch(console.error);
