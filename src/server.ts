ebe25912dc76fde63f2914b9490b1bcea79b56cd
rafaella godinho <rafaellagodinho@outlook.com>
2025-01-02 16:33:18 -0300

    post - create transaction

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify();
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
 
 async function setup() {
   await knex.raw('SELECT 1');
