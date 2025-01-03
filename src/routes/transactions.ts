ee4df6670b777782df3058665bccc9b159fc9dbe
rafaella godinho <rafaellagodinho@outlook.com>
2025-01-03 11:15:41 -0300

    listar transações - todas e por id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import { knex } from "../database"
 import crypto, { randomUUID } from "node:crypto"
 
 export async function transactionsRoutes(app: FastifyInstance) {
+    app.get('/', async () => {
+        const transactions = await knex('transacoes').select()
+
+        return { transactions }
+    })
+
+    app.get('/:id', async (request) => {
+        const getTransactionsParamsSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = getTransactionsParamsSchema.parse(request.params)
+
+        const transaction = await knex('transacoes').where('id', id).first()
+
+        return { transaction }
+    })
+
     app.post('/', async (request, reply) => {
         // {title, amount, type: credit or debit}
         const createTransactionBodySchema = z.object({
